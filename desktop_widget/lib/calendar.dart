bd0d05e62533ee5312e04ed2c8ffb3338753797c
unvictory2 <seungeon7878@gmail.com>
2024-08-22 23:57:35 +0900

    메뉴 상단 공백 제거

--- desktop_widget/lib/calendar.dart
+++ desktop_widget/lib/calendar.dart
@@ -22,7 +22,8 @@ class _CalendarState extends State<Calendar> {
     return Scaffold(
       body: MonthView(
         useAvailableVerticalSpace: true, // 잘리는 거 방지
-        onCellTap: (events, date, offset) { // 날짜 터치시 해당 날짜 일정 메뉴로 띄움
+        onCellTap: (events, date, offset) {
+          // 날짜 터치시 해당 날짜 일정 메뉴로 띄움
           _showDailyEvent(context, events, date, offset);
         },
       ),
@@ -31,7 +32,6 @@ class _CalendarState extends State<Calendar> {
 
   void _showDailyEvent(BuildContext context,
       List<CalendarEventData<Object?>> events, DateTime date, Offset offset) {
-
     // // 화면 안에 메뉴 띄우기 위해 오버레이 정보 가져옴
     // final RenderBox overlay =
     //     Overlay.of(context).context.findRenderObject() as RenderBox;
@@ -46,71 +46,50 @@ class _CalendarState extends State<Calendar> {
 
     String weekdayDate = _formatDate(date);
 
-//TODO : 마진 손봐서 메뉴 상단 색 바꾸기, 메뉴 내부 내용 수정, 및 디자인 고치기
-
     showMenu(
       context: context,
       position: position,
+      menuPadding: EdgeInsets.zero, // 간격 종류가 여러 갠데 그 중 하나 제거
       items: [
-        PopupMenuItem(
-      padding: EdgeInsets.zero, // 간격 제거
-      child: Container(
-        width: double.infinity, // 전체 너비 차지
-        decoration: BoxDecoration(
-          color: Colors.blueGrey, // Topbar 배경색
-          borderRadius: BorderRadius.vertical(top: Radius.circular(8)), // 위쪽 테두리 둥글게
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Row(
-            children: [
-              Expanded(
-                flex: 4,
-                child: Text(
-                  weekdayDate + ' 일정',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: IconButton(
-                  icon: Icon(Icons.add_outlined, color: Colors.white),
-                  onPressed: () {
-                    // 일정 추가 메뉴
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-        PopupMenuItem(
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Text(weekdayDate + ' 일정'),
-                  ), // 8.20(화) 일정
-                Expanded(
-                  flex: 1,
-                  child: IconButton(
-                    icon: Icon(Icons.add_outlined),
-                    onPressed: () {
-                      // 일정 추가 메뉴
-                    },
+        PopupMenuItem( // Topbar 
+          padding: EdgeInsets.zero, // 간격 제거
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.blueGrey, // Topbar 배경색
+              borderRadius:
+                  BorderRadius.vertical(top: Radius.circular(8)), // 위쪽 테두리 둥글게
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Text(
+                      weekdayDate + ' 일정',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                )
-              ],
+                  Expanded(
+                    flex: 1,
+                    child: IconButton(
+                      icon: Icon(Icons.add_outlined, color: Colors.white),
+                      onPressed: () {
+                        // 일정 추가 메뉴
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
+        ),
 
-         // CalendarEventData 리스트를 순회하며 PopupMenuItem 생성
-    ...events.map((event) => PopupMenuItem(
-      child: Text(event.title),
-      value: event.title,
-    )),  
-
+        // CalendarEventData 리스트를 순회하며 PopupMenuItem 생성
+        ...events.map((event) => PopupMenuItem(
+              child: Text(event.title),
+              value: event.title,
+            )),
       ],
     ).then((value) {
       if (value != null) {
